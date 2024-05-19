296fa7543e6c425e7480febaf5525f9039b4abe1
azielsam <azielsam14@gmail.com>
2024-05-19 23:55:03 +0800

    Feat

--- consts/consts.go
+++ consts/consts.go
@@ -13,11 +13,11 @@ import (
 
 const (
 	// TODO: choose a human-readable part for your hyperchain
-	HRP = ""
+	HRP = "Aziel"
 	// TODO: choose a name for your hyperchain
-	Name = ""
+	Name = "AZ"
 	// TODO: choose a token symbol
-	Symbol = ""
+	Symbol = "AZcoin"
 )
 
 var ID ids.ID
